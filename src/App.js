ce553e93d055f2d138c9b10912113bb1fad1f3f4
Lawrence <Lawrence4code@gmail.com>
2020-05-26 11:04:58 +0530

    add checkout page, create checkout item and update selector

--- src/App.js
+++ src/App.js
@@ -8,11 +8,11 @@ import './App.css';
 import Header from './components/header/header.component';
 import HomePage from './pages/homepage/homepage.component';
 import ShopPage from './pages/shop/shop.component';
+import CheckoutPage from './pages/checkout/checkout.component';
 import SignInAndSignUpPage from './pages/sign-in-and-sign-up/sign-in-and-sign-up';
 import { auth, createUserProfileDocument } from './firebase/firebase.util';
 import { setCurrentUser } from './redux/user/user.action';
 import { selectCurrentUser } from './redux/user/user.selector';
-
 class App extends React.Component {
   constructor() {
     super();
@@ -54,6 +54,7 @@ class App extends React.Component {
         <Switch>
           <Route exact path='/' component={HomePage} />
           <Route path='/shop' component={ShopPage} />
+          <Route exact path='/checkout' component={CheckoutPage} />
           <Route exact path='/signin' render={() => this.props.currentUser ? <Redirect to='/'/> : <SignInAndSignUpPage /> }/>
         </Switch>
       </div>
--- src/redux/cart/cart.selector.js
+++ src/redux/cart/cart.selector.js
@@ -12,6 +12,11 @@ export const selectCartItemsCount = createSelector(
   cartItems => cartItems.reduce((acc, item) => acc + item.quantity, 0)
 )
 
+export const selectCartTotal = createSelector(
+  [selectCartItems],
+  cartItems => cartItems.reduce((acc, item) => acc + item.quantity * item.price, 0)
+)
+
 export const selectCartHidden = createSelector(
   [selectCart],
   cart => cart.hidden
