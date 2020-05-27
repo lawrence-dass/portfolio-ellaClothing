92d0fbefeb0d10ebee3d53b14254ac2ab4217a3a
Lawrence <Lawrence4code@gmail.com>
2020-05-27 09:23:20 +0530

    feat: store directory and shop data in state and create collection-overview component and saparate the logic

--- src/index.js
+++ src/index.js
@@ -2,8 +2,11 @@ import React from 'react';
 import ReactDOM from 'react-dom';
 import { BrowserRouter as Router } from 'react-router-dom';
 import { Provider } from 'react-redux';
+import { PersistGate } from 'redux-persist/integration/react'
 
-import store from './redux/store';
+import {store, persistor } from './redux/store';
+
+
 import App from './App';
 import './index.css';
 
@@ -11,7 +14,9 @@ ReactDOM.render(
   <Provider store={store}>
     <React.StrictMode>
       <Router>
+        <PersistGate persistor={persistor}>
         <App />
+        </PersistGate>
       </Router>
     </React.StrictMode>
   </Provider>,
