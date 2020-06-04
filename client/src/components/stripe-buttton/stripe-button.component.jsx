1cb84e6b3dd96cabfb94b43439f548a494685ff2
Lawrence <Lawrence4code@gmail.com>
2020-06-04 17:27:36 +0530

    Add server for stripe api integration

--- client/src/components/stripe-buttton/stripe-button.component.jsx
+++ client/src/components/stripe-buttton/stripe-button.component.jsx
@@ -1,13 +1,24 @@
 import React from 'react';
 import StripeCheckout from 'react-stripe-checkout';
+import axios from 'axios';
 
 const StripeCheckoutButton = ({ price }) => {
   const priceForStripe = price * 100;
   const publishableKey = 'pk_test_HDbbijqw2m4EYwXGs43GtM2M';
 
   const onToken = token => {
-    console.log(token)
-    alert('Payment Sucessful!');
+    axios({
+      url: 'payment',
+      method: 'post',
+      data: {
+        amount: priceForStripe,
+        token
+      }
+    }).then(response => {
+      alert('Payment Success')
+    }).catch((err) => {
+      alert("Payment Error");
+    })
   }
 
   return (
