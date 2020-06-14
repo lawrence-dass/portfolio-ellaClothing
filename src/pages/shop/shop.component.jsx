749819d1899a8b6095c31479ae40ff20c7df035e
Lawrence <Lawrence4code@gmail.com>
2020-06-14 18:31:43 +0530

    fix: lazy load child routes

--- src/pages/shop/shop.component.jsx
+++ src/pages/shop/shop.component.jsx
@@ -1,12 +1,12 @@
-import React, { useEffect } from 'react';
+import React, { useEffect, lazy, Suspense  } from 'react';
 import { Route } from 'react-router-dom';
 import { connect } from 'react-redux';
 
-import CollectionsOverviewContainer from '../../components/collection-overview/collection-overview.container';
-import CollectionsPageContainer from '../collection/collection.container';
 import { fetchCollectionsStart } from '../../redux/shop/shop.actions';
-
 import { ShopContainer } from './shop.styles';
+
+const CollectionsOverviewContainer = lazy(() => import('../../components/collection-overview/collection-overview.container'));
+const CollectionsPageContainer = lazy(() => import('../collection/collection.container'));
 
 const ShopPage = ({ fetchCollectionsStart, match }) => {
   useEffect(() => {
@@ -15,6 +15,7 @@ const ShopPage = ({ fetchCollectionsStart, match }) => {
 
   return (
     <ShopContainer>
+      <Suspense>
       <Route
         exact
         path={`${match.path}`}
@@ -24,6 +25,7 @@ const ShopPage = ({ fetchCollectionsStart, match }) => {
         path={`${match.path}/:collectionId`}
         component={CollectionsPageContainer}
       />
+      </Suspense>
     </ShopContainer>
   );
 };
