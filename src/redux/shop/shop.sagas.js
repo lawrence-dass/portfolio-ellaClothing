13d84012f1e7063a1ab00181236c1092399f2b72
Lawrence <Lawrence4code@gmail.com>
2020-06-16 21:21:37 +0530

    fix: remove dead code

--- src/redux/shop/shop.sagas.js
+++ src/redux/shop/shop.sagas.js
@@ -5,20 +5,14 @@ import { fetchCollectionsSuccess, fetchCollectionsFailure } from './shop.actions
 
 import shopActionTypes from './shop.types';
 
-export function* fetchCollectionsAsync () {
+export function* fetchCollectionsAsync() {
 
-  // convert promise based to redux sagas
-  // collectionRef.get().then( snapshot => {
-  //   const collectionsMap = convertCollectionsSnapshotToMap(snapshot);
-  //   dispatch(fetchCollectionsSuccess(collectionsMap));
-  // }).catch((err) => dispatchEvent(fetchCollectionsFailure(err.message)))
-  
   try {
     const collectionRef = firestore.collection('collections');
     const snapshot = yield collectionRef.get();
     const collectionsMap = yield call(convertCollectionsSnapshotToMap, snapshot);
     yield put(fetchCollectionsSuccess(collectionsMap));
-  } catch(error) {
+  } catch (error) {
     yield put(fetchCollectionsFailure(error.message));
   }
 }
@@ -27,6 +21,6 @@ export function* fetchCollectionStart() {
   yield takeLatest(shopActionTypes.FETCH_COLLECTION_START, fetchCollectionsAsync)
 }
 
-export function* shopSagas () {
+export function* shopSagas() {
   yield all([call(fetchCollectionStart)])
 }
